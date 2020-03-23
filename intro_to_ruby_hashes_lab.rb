b7ea799be29f806bcc8e3e061245ef5b484b5fce
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-23 23:22:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,18 +53,8 @@ def monopoly_with_fourth_tier
       pennsylvania_railroad: {mortgage_value: 200},
       b_and_o_railroad: {mortgage_value: 300},
       shortline_railroad: {mortgage_value: 400},
+          },
+      }
     }
-  }
-  }
-  end
-
-
-
-
-
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
+}
 end
